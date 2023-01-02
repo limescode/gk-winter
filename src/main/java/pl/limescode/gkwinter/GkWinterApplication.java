45f5c225c3351ffb90bc8c75584f8d9c2f083c80
limescode <katechizis@gmail.com>
2023-01-02 04:29:12 +0100

    initial commit

---
+++ src/main/java/pl/limescode/gkwinter/GkWinterApplication.java
@@ -0,0 +1,13 @@
+package pl.limescode.gkwinter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GkWinterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GkWinterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/limescode/gkwinter/GkWinterApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.limescode.gkwinter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GkWinterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
